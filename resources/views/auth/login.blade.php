a73b4ee8213b1f600d29e9d4f047ac4764b57169
NAOHIROITO2178 <korokoro78@ezweb.ne.jp>
2022-05-03 01:25:28 +0900

    Googleログインボタンをコメントアウト化

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -43,11 +43,11 @@
                     {{ __('Log in') }}
                 </x-jet-button>
             </div>
-            <div class="flex items-center justify-end mt-4">
+            {{-- <div class="flex items-center justify-end mt-4">
                 <a href="{{ url('login/google') }}">
                     <img src="https://developers.google.com/identity/images/btn_google_signin_dark_normal_web.png" style="margin-left: 3em;">
                 </a>
-            </div>
+            </div> --}}
         </form>
     </x-jet-authentication-card>
 </x-guest-layout>
